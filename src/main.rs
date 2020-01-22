37eb2ac214a7ac13d50bf5637b45a905a9244b7f
Gaurav Tyagi <tyagig@google.com>
2020-01-22 22:07:34 +0530

    Clean main

--- src/main.rs
+++ src/main.rs
@@ -6,63 +6,71 @@ mod cmd_generator;
 
 use cmd_generator::base::{Cmd, CmdType};
 
+// const Base path to the application.
 const BASE_PATH: &str = "java/com/google/social/boq/blogger";
 
+// Main function to start Application CLI.
 fn main() {
-    ///////////// CLAP START //////////////////////////
+    // Define what this CLI app required.
     let matches = App::new("rick")
         .version("0.1v")
-        .about("Doing ricky things.")
+        .about("Vaba laba dub dub.")
         .author("Gaurav Tyagi")
         .arg(
             Arg::with_name("dir")
                 .short("d")
                 .long("directory")
                 .takes_value(true)
-                .required(true)
-                .value_name("DIR, ex: [settingsweb|postweb/postsettings]"),
-        )
-        .arg(Arg::with_name("explain").short("e").long("explain"))
+                .help("Set a dir to run."),
+            )
+        .arg(
+            Arg::with_name("explain")
+                .short("e")
+                .long("explain")
+                .help("Explain what command it going to run."),
+            )
         .arg(
             Arg::with_name("run")
                 .short("r")
                 .long("run")
                 .takes_value(true)
-                .value_name("RUN, ex: [jswire,debug|build]"),
+                .value_name("j: jswire, d: debug, t: testcases")
+                .help("run define test cases you want to execute, ex: -r=j,d")
         )
         .arg(
             Arg::with_name("iblaze")
                 .short("i")
                 .long("iblaze")
-                .value_name("Enable debug mode for jswire and jstest"),
+                .value_name("Not Implemented :( WIP..."),
         )
         .arg(
             Arg::with_name("command")
                 .short("cmd")
                 .long("command")
                 .takes_value(true)
-                .value_name("Command, give command to execute"),
+                .value_name("give a command to execute")
+                .help("Run your custom command"),
         )
         .get_matches();
-    ///////////// END ////
 
     let dir = matches.value_of("dir").unwrap();
     let run_for = matches.value_of("run").unwrap_or("all");
     let cmd = Cmd::new(String::from(BASE_PATH), String::from(dir), true);
     let mut generated_cmds: Vec<String> = Vec::new();
-    
+    // Push commands obj to Vec.
     for run in run_for.split(",") {
-        if run == "jswire" || run == "all" {
+        if run == "j" || run == "all" {
             generated_cmds.push(cmd.create_build_cmd(CmdType::Jswire));
         }
-        if run == "build" || run == "all" {
+        if run == "b" || run == "all" {
             generated_cmds.push(cmd.create_build_cmd(CmdType::Build));
         }
-        if run == "jstest" || run == "all" {
+        if run == "t" || run == "all" {
             generated_cmds.push(cmd.create_build_cmd(CmdType::JsTest));
         }
     }
-    // run a custom command only.
+
+    // Check/Run a custom command.
     if matches.is_present("command") {
         generated_cmds.clear();
         let mut command_list = matches.value_of("command").unwrap().split(",");
@@ -70,6 +78,7 @@ fn main() {
             generated_cmds.push(x.to_string());
         }
     }
+    // Print and exit.
     if matches.is_present("explain") {
         explain(generated_cmds);
         return;
@@ -77,6 +86,7 @@ fn main() {
     run_commands(generated_cmds);
 }
 
+// Run a command as process and prints its stdio.
 fn run_commands(cmds: Vec<String>) {
     for cmd in cmds {
 
@@ -92,6 +102,7 @@ fn run_commands(cmds: Vec<String>) {
     }
 }
 
+// Prints all the commands from Vec.
 fn explain(cmds: Vec<String>) {
     for cmd in cmds {
         println!(">>{}", cmd.yellow());
