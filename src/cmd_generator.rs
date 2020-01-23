8b279e30d173004ffb82b28c0e627450d97283dc
Gaurav Tyagi <tyagig@google.com>
2020-01-23 11:31:24 +0530

    correct some command with real time test

--- src/cmd_generator.rs
+++ src/cmd_generator.rs
@@ -1,7 +1,6 @@
 pub mod base {
     #[derive(Debug)]
     pub enum CmdType {
-        All,
         Build,
         Jswire,
         JsTest
@@ -21,26 +20,18 @@ pub mod base {
 
         pub fn create_build_cmd(&self, cmd_type: CmdType) -> String {
             let mut param = String::new();
-            let mut cmd = String::from("blaze build");
             match cmd_type {
-                CmdType::All => {
-                    param = format!(" {}/..", self.base_path);
-                },
                 CmdType::Build => {
-                    param = format!(" {}/{}/..",self.base_path, self.dir);
+                    param = format!("blaze build {}/{}/...",self.base_path, self.dir);
                 }
                 CmdType::Jswire => {
-                    param = format!(" {}/{}/{}/..", self.base_path, self.dir, "jswire");
+                    param = format!("blaze build {}/{}/{}/...", self.base_path, self.dir, "jswire");
                 }
                 CmdType::JsTest => {
-                    param = format!(" {}/{}:{}", self.base_path, self.dir, "test_debug_1");
-                }
-                _ => {
-                    println!("Command type not found.");
+                    param = format!("blaze test {}/{}:{}", self.base_path, self.dir, "jstests_1_debug");
                 }
             }
-            cmd.push_str(&param);
-            cmd
+            param
         }
     }
 }
